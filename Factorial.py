2edfcbddfdf2001a5748f03faf7186935796a301
Kasyap <49114820+srikarkasyap@users.noreply.github.com>
2020-04-24 16:47:36 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+
+a=int(input("Enter a number"))
+fact = 1
+if a==0:
+	print("Factorial is 1")
+for i in range(1, a+1):
+    fact=fact*i
+print(fact)
+
+
---
+++ Reverse_of_digits.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number"))
+
+c=0
+b=a%10
+a=a/10
+b=b*10
+c=b+a
+print("It's reverse is ",c)
