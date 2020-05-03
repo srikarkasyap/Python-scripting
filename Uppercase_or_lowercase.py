fc79ffc19a8605f54703b66d1485c0140b8f967d
Kasyap <49114820+srikarkasyap@users.noreply.github.com>
2020-05-03 14:43:53 +0530

    Add files via upload

---
+++ Uppercase_or_lowercase.py
@@ -0,0 +1,10 @@
+
+val=input("Enter a char:")
+if val>='A'and val<='Z':
+	print("It's a upper case char.")
+
+elif val>='a' and val<= 'z' :
+	print("It's a lower case char.") 
+else
+	print("Enter a proper char.")
+ 
