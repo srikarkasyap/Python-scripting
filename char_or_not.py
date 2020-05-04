e081fb6bda1084f6a73808cf6f444a9f34335c2e
Kasyap <49114820+srikarkasyap@users.noreply.github.com>
2020-05-04 09:47:23 +0530

    Add files via upload

---
+++ char_or_not.py
@@ -0,0 +1,10 @@
+
+val=input("Enter a char:")
+if val>='A'and val<='Z' or val>='a' and val<= 'z' :
+	print("It's a char.")
+
+elif val=='!'or'@'or'#'or'$'or'%'or'^'or'&'or'*'or'_'or'('or')':
+	print("It's a char.")
+
+else :
+	print("It's not a char.")  
