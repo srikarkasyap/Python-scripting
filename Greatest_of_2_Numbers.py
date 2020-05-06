0c4272c564afb3e610113c39021968daa34ead42
Kasyap <49114820+srikarkasyap@users.noreply.github.com>
2020-05-06 11:37:36 +0530

    Add files via upload

---
+++ Greatest_of_2_Numbers.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a integer."))
+b=int(input("Enter another integer."))
+
+if a<b:
+	print("The greatest is:",b)
+elif a>b:
+	print("The greatest is:",a)
+elif a==b:
+	print("Both are equal.")
+else:
+	print("Error!")
