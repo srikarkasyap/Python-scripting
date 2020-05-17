2033474a39052ae1732c75989b8c24a968ad537b
Kasyap <49114820+srikarkasyap@users.noreply.github.com>
2020-05-17 11:07:49 +0530

    Add files via upload

---
+++ Greatest_of_3_Numbers.py
@@ -0,0 +1,12 @@
+a=float(input("Enter a number:"))
+b=float(input("Enter 2nd number:"))
+c=float(input("Enter 3rd number:"))
+
+if a>b and a>c:
+	print("The greatest number is ",a)
+
+if b>a and b>c:
+	print("The greatest number is ",b)
+
+if c>a and c>b:
+	print("The greatest number is ",c)
